3164e8c6816103caa2aeb78546de2633cf3593a2
Laudemir <nlaudemir@gmail.com>
2022-03-09 15:01:06 -0300

    fix: change value port

--- server.js
+++ server.js
@@ -4,11 +4,19 @@ const bp = require("body-parser");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
+
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+
+mongoose.connect(
+  `mongodb+srv://${dbUser}:${dbPassword}@cluster0.v2z3i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+);
 
 const userRoute = require("./src/controllers/userRoute");
 app.use("/", userRoute);
@@ -16,15 +24,6 @@ app.use("/", userRoute);
 const urlRoute = require("./src/controllers/urlRoute");
 app.use("/", urlRoute);
 
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASSWORD;
-
-mongoose
-  .connect(
-    `mongodb+srv://${dbUser}:${dbPassword}@cluster0.v2z3i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-  )
-  .then(() => {
-    app.listen(3000);
-    console.log("Running");
-  })
-  .catch((err) => console.log(err));
+app.listen(PORT, () => {
+  console.log(`http://localhost:3000/${PORT}`);
+});
