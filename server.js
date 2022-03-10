53fbe84e9441c936e2604147cfad42829d0f99d6
Laudemir <nlaudemir@gmail.com>
2022-03-10 02:06:38 -0300

    fix: release procfile

--- server.js
+++ server.js
@@ -15,15 +15,15 @@ app.use(express.json());
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 
+const userRoute = require("./controllers/userRoute");
+app.use("/", userRoute);
+
+const urlRoute = require("./controllers/urlRoute");
+app.use("/", urlRoute);
+
 mongoose.connect(
   `mongodb+srv://${dbUser}:${dbPassword}@cluster0.v2z3i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 );
-
-const userRoute = require("./src/controllers/userRoute");
-app.use("/", userRoute);
-
-const urlRoute = require("./src/controllers/urlRoute");
-app.use("/", urlRoute);
 
 app.listen(port, () => {
   console.log(`App is running at the port ${port}`);
