93166643d90f43950c94d8dfb2f669525ffffd16
Laudemir <nlaudemir@gmail.com>
2022-03-10 01:45:52 -0300

    fix: release procfile

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
+
 const express = require("express");
 const bp = require("body-parser");
-const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const PORT = process.env.PORT;
+
+const app = express();
 
 app.use(cors());
 app.use(bp.json());
