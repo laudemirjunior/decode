4421f1e3f99b36fe9b11c71ea29ebb022a08b97f
Laudemir <nlaudemir@gmail.com>
2022-03-11 15:50:51 -0300

    feat: add more response

--- src/controllers/urlRoute.js
+++ src/controllers/urlRoute.js
@@ -52,13 +52,17 @@ router.get("/:id", async (req, res, next) => {
   if (url) {
     url.hits++;
     await url.save();
-    if (url.url.split("/")[0] === "https:" || url.url.split(".")[0] === "www") {
+    if (
+      url.url.split("//")[0] === "https:" ||
+      url.url.split(".")[0] === "www"
+    ) {
       return res.redirect(url.url);
     } else {
-      return res.redirect(`https://delc.herokuapp.com/${url.url}`);
+      return res.redirect(`https://${url.url}`);
     }
+  } else {
+    return res.status(404).json({ msg: "not found" });
   }
-  return res.status(404).json({ msg: "not found" });
 });
 
 router.get("/url/:id", async (req, res, next) => {
